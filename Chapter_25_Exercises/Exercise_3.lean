1645c6398e3b1d139da71011748efddf51626f7f
Adil Raza <adilraza9000@gmail.com>
2023-07-08 14:58:49 +0100

    Updated Exercise 3

--- Chapter_25_Exercises/Exercise_3.lean
+++ Chapter_25_Exercises/Exercise_3.lean
@@ -3,12 +3,17 @@ import Mathlib.Algebra.Group.Defs
 --- part a
 
 example {G : Type u_1} [Group G] (a : G) : (a⁻¹)⁻¹ = a := by
-  rw [← mul_one (a⁻¹)⁻¹, ← mul_left_inv a, ← mul_assoc, mul_left_inv, one_mul]
+  calc
+    (a⁻¹)⁻¹ = (a⁻¹)⁻¹ * a⁻¹*a := by
+      rw [mul_assoc,mul_left_inv, mul_one]
+    _ = a := by
+      rw [mul_left_inv,one_mul]
 
 --- part b
 
 example {G : Type u_1} [Group G] (a b : G) : (a*b)⁻¹ = b⁻¹*a⁻¹ := by
   have h₁ : (a*b)*(b⁻¹*a⁻¹) = 1 := by
-    rw [← mul_assoc, mul_assoc a, mul_right_inv, mul_one,mul_right_inv,]
-  apply inv_eq_of_mul_eq_one_right
+    rw [← mul_assoc, mul_assoc a, mul_right_inv, mul_one,mul_right_inv]
+  apply left_inv_eq_right_inv
+  exact mul_left_inv (a * b)
   exact h₁
