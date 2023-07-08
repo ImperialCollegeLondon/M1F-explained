56042d2fcdbaf515e685120a3e9ae509fd42062a
Adil Raza <adilraza9000@gmail.com>
2023-07-08 14:59:40 +0100

    small tweaks

--- Chapter_25_Exercises/Exercise_3.lean
+++ Chapter_25_Exercises/Exercise_3.lean
@@ -1,6 +1,12 @@
 import Mathlib.Algebra.Group.Defs
 
---- part a
+/-
+Let G be a group, and let a, b ∈ G. Prove that
+  i) (a⁻¹)⁻¹ = a
+  ii) (a * b)⁻¹ = b⁻¹ * a⁻¹
+-/
+
+--- part i)
 
 example {G : Type u_1} [Group G] (a : G) : (a⁻¹)⁻¹ = a := by
   calc
@@ -9,10 +15,10 @@ example {G : Type u_1} [Group G] (a : G) : (a⁻¹)⁻¹ = a := by
     _ = a := by
       rw [mul_left_inv,one_mul]
 
---- part b
+--- part ii)
 
-example {G : Type u_1} [Group G] (a b : G) : (a*b)⁻¹ = b⁻¹*a⁻¹ := by
-  have h₁ : (a*b)*(b⁻¹*a⁻¹) = 1 := by
+example {G : Type u_1} [Group G] (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
+  have h₁ : (a * b)*(b⁻¹ * a⁻¹) = 1 := by
     rw [← mul_assoc, mul_assoc a, mul_right_inv, mul_one,mul_right_inv]
   apply left_inv_eq_right_inv
   exact mul_left_inv (a * b)
