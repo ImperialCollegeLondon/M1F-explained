2042ad203903ee230b279d3843048ca7bb538831
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-07-17 19:53:00 +0100

    fix ex 5

--- M1FExplained/Chapter08/Exercises/Exercise01.lean
+++ M1FExplained/Chapter08/Exercises/Exercise01.lean
@@ -1,7 +1,8 @@
-import Mathbin.Tactic.Default
+import Mathlib.Tactic
 
 namespace Chapter08.Exercise01
 
+example (b : ℕ) (hb : 1 ≤ b) : ∃ c, b = c + 1 := by exact Nat.exists_eq_add_of_le' hb
 /-
 Prove by induction that it's possible to pay, without requiring
 change, any whole number of roubles greater than 7 with
@@ -13,31 +14,41 @@ example (n : ℕ) (hn : 7 < n) : ∃ a b : ℕ, n = 3 * a + 5 * b :=
   -- to think about exactly what statement you want to prove
   -- by induction.
   generalize hd : n - 8 = d
-  have hnd : n = d + 8 := by linarith
+  have hnd : n = d + 8 := Iff.mp (Nat.sub_eq_iff_eq_add hn) hd
   · rw [hnd]
     clear hnd hd hn n
     induction' d with k hk
     · use 1
       use 1
-      norm_num
     · change ∃ a b, k + 1 + 8 = 3 * a + 5 * b
       rcases hk with ⟨a, b, hp⟩
       by_cases h : 0 < b
       · use a + 2
         use b - 1
+        change k + 8 + 1 = _
+        rw [hp]
+        ring_nf
+        change 1 ≤ b at h
+        replace h := Nat.exists_eq_add_of_le' h
+        rcases h with ⟨bb, rfl⟩  
+        simp
         linarith
       · push_neg at h 
         simp only [nonpos_iff_eq_zero] at h 
         subst h
         norm_num at hp 
-        have hk : 6 < k + 8 := by omega
+        have hk : 6 < k + 8 := by linarith
         rw [hp] at hk 
         have h3 : 0 < 3 := by simp
         have ha : 2 < a := (mul_lt_mul_left h3).mp hk
         refine' ⟨a - 3, 2, _⟩
-        ring_nf
         change k + 8 + 1 = 3 * (a - 3) + 10
         rw [hp]
+        replace ha := Nat.exists_eq_add_of_lt ha
+        rcases ha with ⟨k, hk⟩
+        rw [add_comm 2 k] at hk
+        subst hk
+        simp   
         linarith
 
 end Chapter08.Exercise01
--- M1FExplained/Chapter08/Exercises/Exercise05.lean
+++ M1FExplained/Chapter08/Exercises/Exercise05.lean
@@ -1,5 +1,5 @@
-import Mathbin.Tactic.Default
-import Mathbin.Data.Real.Basic
+import Mathlib.Tactic
+import Mathlib.Data.Real.Basic
 
 -- for part d
 -- for part d
@@ -10,12 +10,12 @@ theorem part_a (n : ℕ) : (11 : ℤ) ∣ 5 ^ (2 * n) - 3 ^ n :=
   induction' n with x hx
   · norm_num
   rw [Nat.succ_eq_add_one, mul_add, pow_add, mul_one]
-  nth_rw 2 [pow_succ]
+  rw [pow_succ 3]
   cases' hx with m hm
   have q : (5 : ℤ) ^ 2 = (22 : ℤ) + (3 : ℤ); norm_num
   rw [q, mul_add]
-  nth_rw 3 [mul_comm]
-  have p :
+  --nth_rw 3 [mul_comm]
+  have _ :
     (3 : ℤ) * (5 : ℤ) ^ (2 * x) - (3 : ℤ) * (3 : ℤ) ^ x =
       (3 : ℤ) * ((5 : ℤ) ^ (2 * x) - (3 : ℤ) ^ x) :=
     by rw [mul_sub _]
@@ -51,8 +51,8 @@ theorem part_d (n : ℕ) (x : ℝ) (hx : 2 ≤ x) : (n : ℝ) * x ≤ x ^ n :=
   induction' n with h hh
   · norm_num
   by_cases hn : h = 0
-  · rw [hn]
-    simp only [Nat.cast_one, one_mul, pow_one]
+  · subst hn 
+    simp [show Nat.succ 0 = 1 from rfl]
   · rw [pow_succ, Nat.cast_succ, mul_comm]
     rw [mul_le_mul_left]
     have p : ↑h + 1 ≤ ↑h * x :=
@@ -65,23 +65,22 @@ theorem part_d (n : ℕ) (x : ℝ) (hx : 2 ≤ x) : (n : ℝ) * x ≤ x ^ n :=
         norm_cast
         rw [mul_two]
         simp only [add_le_add_iff_left]
-        exact nat.one_le_iff_ne_zero.mpr hn
+        exact Nat.one_le_iff_ne_zero.mpr hn
       exact le_trans q g
     apply le_trans p
     exact hh
     linarith
 
-theorem part_e (n : ℕ) (hn : 3 ≤ n) : 4 ^ n + 3 ^ n + 2 ^ n < 5 ^ n :=
-  by
+theorem part_e (n : ℕ) (hn : 3 ≤ n) : 4 ^ n + 3 ^ n + 2 ^ n < 5 ^ n := by
   induction' n with h hx
   norm_num at hn 
-  repeat' rw [pow_succ]
+  repeat' rw [Nat.succ_eq_add_one, pow_succ]
   have p : 2 * 2 ^ h < 5 * 2 ^ h := by
-    simp only [mul_lt_mul_right, pow_pos, Nat.succ_pos', Nat.bit0_lt_bit1_iff, Nat.one_le_bit0_iff]
+    simp only [mul_lt_mul_right, pow_pos, Nat.succ_pos']
   have q : 3 * 3 ^ h < 5 * 3 ^ h := by
-    simp only [mul_lt_mul_right, pow_pos, Nat.succ_pos', bit1_lt_bit1, Nat.one_lt_bit0_iff]
+    simp only [mul_lt_mul_right, pow_pos, Nat.succ_pos']--, bit1_lt_bit1, Nat.one_lt_bit0_iff]
   have r : 4 * 4 ^ h < 5 * 4 ^ h := by
-    simp only [mul_lt_mul_right, pow_pos, Nat.succ_pos', Nat.bit0_lt_bit1_iff]
+    simp only [mul_lt_mul_right, pow_pos, Nat.succ_pos']--, Nat.bit0_lt_bit1_iff]
   have t : 5 * 4 ^ h + 5 * 3 ^ h + 5 * 2 ^ h = 5 * (4 ^ h + 3 ^ h + 2 ^ h) := by ring_nf
   have s := add_lt_add (add_lt_add r q) p
   rw [t] at s 
@@ -90,13 +89,16 @@ theorem part_e (n : ℕ) (hn : 3 ≤ n) : 4 ^ n + 3 ^ n + 2 ^ n < 5 ^ n :=
     norm_num
   · have hhh : 3 ≤ h := by
       obtain tt := Nat.of_le_succ hn
-      have := nat.succ_ne_succ.mpr hh
+      have := Nat.succ_ne_succ.mpr hh
       norm_num at this 
-      cases tt
+      rcases tt with (tt | tt)
       · exact tt
       · exfalso
+        replace tt := Nat.succ_injective tt
         exact this tt.symm
+    rw [pow_succ 3, pow_succ 2]
     apply lt_trans s
+    rw [pow_succ 5]
     rw [mul_lt_mul_left]
     · exact hx hhh
     norm_num
