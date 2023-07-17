66bceefb35af7903b19d48c772f6772282e7c0e3
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-07-17 11:03:15 +0100

    tidy

--- M1FExplained/Chapter01/Exercises/Exercise06.lean
+++ M1FExplained/Chapter01/Exercises/Exercise06.lean
@@ -12,7 +12,7 @@ theorem part_a : Real.sqrt 6 - Real.sqrt 2 > 1 :=
   by
   have a := Real.sqrt_nonneg (6 : Real)
   have b := Real.sqrt_nonneg (2 : Real)
-  have c : 2 * Real.sqrt 2 ≥ 0 := by linarith
+  have _ : 2 * Real.sqrt 2 ≥ 0 := by linarith
   suffices Real.sqrt 6 > 1 + Real.sqrt 2 by linarith
   suffices Real.sqrt 6 ^ 2 > (1 + Real.sqrt 2) ^ 2 by exact lt_of_pow_lt_pow 2 a this
   rw [Real.sq_sqrt, add_sq 1 (Real.sqrt 2), Real.sq_sqrt]
