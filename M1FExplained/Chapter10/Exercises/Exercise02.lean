419726ba6b3089c97c2d94bff472a3dce2076a75
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-07-22 16:27:50 +0100

    C10E02 part a statment

---
+++ M1FExplained/Chapter10/Exercises/Exercise02.lean
@@ -0,0 +1,12 @@
+import Mathlib.Tactic
+
+namespace Chapter10.Exercise02
+
+lemma part_a (a b d : ℕ) (hd : d = Nat.gcd a b) : ∃ (s t : ℕ),  d = s * a - t * b := by sorry
+
+
+
+
+
+
+end Chapter10.Exercise02
