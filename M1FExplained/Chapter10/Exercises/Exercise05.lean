1b339ac95c7e1bc6489af7f806c35db175aa34a0
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-07-31 17:13:26 +0100

    Ex05 documentation + status update

--- M1FExplained/Chapter10/Exercises/Exercise05.lean
+++ M1FExplained/Chapter10/Exercises/Exercise05.lean
@@ -17,7 +17,9 @@ lemma part_a (m n a : ℤ) (hmn : Int.gcd m n = 1) (hm : m ∣ a) (hn : n ∣ a)
   |⟨k₂, hk₂⟩ => 
   let r := Int.gcdA m n
   let s := Int.gcdB m n
+  -- By Bézout's lemma, some linear combination of m, n is equal to one.
   have h₁ : 1 = m * r + n * s := by rw [←Int.gcd_eq_gcd_ab m n, hmn]; norm_cast
+  -- Aim at proving that n ∣ k₁, where m * k₁ = a 
   have h₂ : k₁ = k₁ * m * r + k₁ * n * s := by 
     calc
       k₁ = k₁ * 1 := by ring
@@ -33,6 +35,7 @@ lemma part_a (m n a : ℤ) (hmn : Int.gcd m n = 1) (hm : m ∣ a) (hn : n ∣ a)
     exact h₂ 
   match hc with 
   |⟨e, he⟩ => 
+  -- e is the number such that n * e = k₁. It also is the case that (m * n) * e = a, proving our statment. 
   use e
   calc
     a = k₁ * m := by exact hk₁
@@ -53,6 +56,8 @@ non-zero, then the statment is true, so that is what we will prove.
 To help, we first define two helper lemmas as the solution is longwinded.
 -/
 
+-- helper_1 is a simple inequality involving absolute values.
+
 lemma helper_1 (m n M : ℤ) (hM : 1 < abs M) (hM' : M ∣ m * n) (hm : m ≠ 0) (hn : n ≠ 0) : abs (m * n / M)  < abs (m * n) := by
 match hM' with 
 |⟨k, hk⟩ => 
@@ -72,6 +77,11 @@ rw [hk, abs_mul, Int.mul_ediv_cancel_left]
  have : ¬ 1 < abs M := by rw [hM0]; norm_num
  contradiction
 
+/- 
+helper_2 converts `Nat.not_dvd_of_pos_of_lt` to the integer case, casting to absolute values.
+It says that an integer n cannot divide any non-zero integer m which is smaller in magnitude.
+-/
+
 lemma helper_2 (m n : ℤ) (h1 : 0 < abs m) (h2 : abs m < abs n) : ¬ n ∣ m := by 
   have h1' : 0 < Int.natAbs m := by {
     rw [@Int.natAbs_pos]
@@ -89,14 +99,18 @@ lemma helper_2 (m n : ℤ) (h1 : 0 < abs m) (h2 : abs m < abs n) : ¬ n ∣ m :=
 
 lemma part_b (m n : ℤ) (hm : m ≠ 0) (hn : n ≠ 0) (h2 : Int.gcd m n ≠ 1) : ∃ (a : ℤ), (m ∣ a) ∧ (n ∣ a) ∧ (¬(m * n) ∣ a) := by 
   let M : ℤ := Int.gcd m n
+  /- Since m / M and n / M are both integers, our choice of a has both factors m and n. However m * n > a,
+  so cannot possibly divide it. -/
   use (m * n / M)
   apply And.intro
+  -- First prove that m ∣ a.
   ·rw [show m * n / M = m * (n / M) by {
     exact Int.mul_ediv_assoc m (show M ∣ n by exact Int.gcd_dvd_right m n)
   }]
    have : m ∣ m := by use 1; norm_num
    exact dvd_mul_of_dvd_left this (n / M)
   apply And.intro
+  -- Next, prove n ∣ a.
   ·rw [show m * n / M = n * (m / M) by {
     rw [←mul_comm n m]
     exact Int.mul_ediv_assoc n (show M ∣ m by exact Int.gcd_dvd_left m n)
@@ -110,6 +124,7 @@ lemma part_b (m n : ℤ) (hm : m ≠ 0) (hn : n ≠ 0) (h2 : Int.gcd m n ≠ 1) 
     use k * n
     rw [hk]
     rw [mul_assoc]}
+  -- Finally, we prove that (m * n) cannot divide a, because it is larger.
   ·have h1' : 0 < abs (m * n / M) := by {
     rw [abs_pos]
     intro h
@@ -137,6 +152,7 @@ lemma part_b (m n : ℤ) (hm : m ≠ 0) (hn : n ≠ 0) (h2 : Int.gcd m n ≠ 1) 
      }
      contradiction
    }
+   -- Apply both helper_1 and helper_2 to finish the proof.
    have h2' : abs (m * n / M)  < abs (m * n) := by exact helper_1 m n M h1M hMmn hm hn
    exact helper_2 (m * n / M) (m * n) h1' h2'
 
@@ -151,6 +167,7 @@ lemma part_c (x y m : ℤ) (hx : Int.gcd x m = 1) (hy : Int.gcd y m = 1) : Int.g
   let b := Int.gcdB x m
   let c := Int.gcdA y m
   let d := Int.gcdB y m
+  -- Apply Bézout's lemma to x, m and y, m respectivley.
   have hxm : 1 = x * a + m * b := by rw [←Int.gcd_eq_gcd_ab x m, hx]; norm_cast
   have hym : 1 = y * c + m * d := by rw [←Int.gcd_eq_gcd_ab y m, hy]; norm_cast
   have h   : 1 = a * c * x * y  + (x * a * d + b * y * c + m * b * d) * m := by
@@ -160,6 +177,8 @@ lemma part_c (x y m : ℤ) (hx : Int.gcd x m = 1) (hy : Int.gcd y m = 1) : Int.g
       _ = x * a * y * c + x * a * m * d + m * b * y * c + m * b * m * d := by ring
       _ = a * c * x * y  + (x * a * d + b * y * c + m * b * d) * m := by ring
   rw [←show a * c * (x * y) = a * c * x * y by ring] at h
+  /- We showed in Exercise04 part c that if some linear combination of two integers is equal to one, their gcd
+  is also equal to one, we use this fact here. -/
   exact Chapter10.Exercise04.part_c (a * c) ((x * a * d + b * y * c + m * b * d)) (x * y) m (Eq.symm h)
 
 end Chapter10.Exercise05
