a781400cad4f6cf6ee569a3cf007e91638074c81
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-07-31 16:16:11 +0100

    EX05 DONE

--- M1FExplained/Chapter10/Exercises/Exercise05.lean
+++ M1FExplained/Chapter10/Exercises/Exercise05.lean
@@ -32,7 +32,26 @@ lemma part_a (m n a : ℤ) (hmn : Int.gcd m n = 1) (hm : m ∣ a) (hn : n ∣ a)
     _ = (n * e) * m := by rw [he]
     _ = m * n * e := by ring
 
-lemma helper (m n : ℤ) (h1 : 0 < abs m) (h2 : abs m < abs n) : ¬ n ∣ m := by 
+lemma helper_1 (m n M : ℤ) (hM : 1 < abs M) (hM' : M ∣ m * n) (hm : m ≠ 0) (hn : n ≠ 0) : abs (m * n / M)  < abs (m * n) := by
+match hM' with 
+|⟨k, hk⟩ => 
+have hkn0 : 0 < abs k := by {
+  rw [abs_pos]
+  intro h
+  rw [h] at hk
+  simp at hk
+  tauto
+}
+rw [hk, abs_mul, Int.mul_ediv_cancel_left]
+·calc
+  abs k = 1 * abs k := by norm_num
+  _ < abs M * abs k := by rel [hM]
+·intro h 
+ have hM0 : abs M = 0 := by simp; exact h
+ have : ¬ 1 < abs M := by rw [hM0]; norm_num
+ contradiction
+
+lemma helper_2 (m n : ℤ) (h1 : 0 < abs m) (h2 : abs m < abs n) : ¬ n ∣ m := by 
   have h1' : 0 < Int.natAbs m := by {
     rw [@Int.natAbs_pos]
     exact Iff.mp abs_pos h1
@@ -62,23 +81,42 @@ lemma part_b (m n : ℤ) (hm : m ≠ 0) (hn : n ≠ 0) (h2 : Int.gcd m n ≠ 1) 
   }]
    have : n ∣ n := by use 1; norm_num
    exact dvd_mul_of_dvd_left this (m / M)
+  have : M ∣ m := by exact Int.gcd_dvd_left m n
+  have hMmn : M ∣ m * n := by {
+    match this with
+    |⟨k, hk⟩ => 
+    use k * n
+    rw [hk]
+    rw [mul_assoc]}
   ·have h1' : 0 < abs (m * n / M) := by {
     rw [abs_pos]
     intro h
-    have : M ∣ m := by exact Int.gcd_dvd_left m n
-    have hM : M ∣ m * n := by {
-      match this with
-      |⟨k, hk⟩ => 
-      use k * n
-      rw [hk]
-      rw [mul_assoc]
-    }
-    have := Int.eq_zero_of_ediv_eq_zero hM h
+    have := Int.eq_zero_of_ediv_eq_zero hMmn h
     rw [mul_eq_zero] at this
     tauto
   }
-   have h2' : abs (m * n / M)  < abs (m * n) := by sorry
-   exact helper (m * n / M) (m * n) h1' h2'
+   have h1M : 1 < abs M := by {
+    apply by_contradiction
+    intro h
+    push_neg at h
+    have := @Int.abs_le_one_iff M
+    rw [this] at h
+    apply Or.elim h
+    ·intro hM0
+     simp at hM0
+     rw [Int.gcd_eq_zero_iff] at hM0
+     tauto
+    ·intro
+     | Or.inl hone => simp at hone; contradiction
+     | Or.inr hmone => 
+     have : 0 < M := by {
+      rw [@Int.ofNat_pos]
+      exact Int.gcd_pos_of_ne_zero_left n hm
+     }
+     contradiction
+   }
+   have h2' : abs (m * n / M)  < abs (m * n) := by exact helper_1 m n M h1M hMmn hm hn
+   exact helper_2 (m * n / M) (m * n) h1' h2'
 
 lemma part_c (x y m : ℤ) (hx : Int.gcd x m = 1) (hy : Int.gcd y m = 1) : Int.gcd (x * y) m = 1 := by 
   let a := Int.gcdA x m
