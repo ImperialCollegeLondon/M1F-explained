45aaee2053cb5bf6150b9cff51a50d7f9c651f14
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-01 08:18:19 +0100

    EX04 Part b done

--- M1FExplained/Chapter10/Exercises/Exercise04.lean
+++ M1FExplained/Chapter10/Exercises/Exercise04.lean
@@ -2,10 +2,6 @@ import Mathlib.Tactic
 
 namespace Chapter10.Exercise04
 
-lemma helper (m n : ℤ) (hmn : m * n = 1) : (m = 1) ∨ (m = -1) := by  
-  have h_unit : IsUnit m := by apply Iff.mpr isUnit_iff_exists_inv; use n; exact hmn
-  exact Int.isUnit_eq_one_or h_unit
-
 /- Solve part a by induction and use:
 
 theorem int.mod_eq_mod_iff_mod_sub_eq_zero  {m n k : ℤ} :
@@ -30,9 +26,11 @@ lemma part_a (n : ℕ) (hn : n > 0) : Nat.gcd (6 * n + 8) (4 * n + 5) = 1 := by
       1 = 1 + 12 * n - 12 * n := by norm_num
       _ = 2 * 6 * n + 16 - 3 * 4 * n - 15 := by ring -/
 
+lemma helper (m n : ℤ) (hmn : m * n = 1) : (m = 1) ∨ (m = -1) := by  
+  have h_unit : IsUnit m := by apply Iff.mpr isUnit_iff_exists_inv; use n; exact hmn
+  exact Int.isUnit_eq_one_or h_unit
 
-
-lemma part_b (a b : ℤ) (hzero : a ≠ 0 ∧ b ≠ 0) (hab : a ∣ b) (hba : b ∣ a) : a = b ∨ a = -b := by
+lemma part_b (a b : ℤ) (ha : a ≠ 0) (hb : b ≠ 0) (hab : a ∣ b) (hba : b ∣ a) : a = b ∨ a = -b := by
   match hab with
   |⟨k₁, hk₁⟩ => 
   match hba with
@@ -40,9 +38,9 @@ lemma part_b (a b : ℤ) (hzero : a ≠ 0 ∧ b ≠ 0) (hab : a ∣ b) (hba : b 
   rw [hk₂] at hk₁
   have h₁ : 1 = k₂ * k₁ := by 
     calc
-      1 = b / b := sorry --exact (Eq.symm (Int.div_self hzero.right))
+      1 = b / b := by exact Eq.symm (Int.ediv_self hb)
       _ = (b * k₂ * k₁) / b := by nth_rewrite 1 [hk₁]; rfl
-      _ = k₂ * k₁ := sorry 
+      _ = k₂ * k₁ := by rw [Int.mul_assoc]; exact Int.mul_ediv_cancel_left (k₂ * k₁) hb
   have h₂ : k₂ = 1 ∨ k₂ = -1 := by exact helper k₂ k₁ (Eq.symm h₁)
   apply Or.elim h₂
   ·intro h
@@ -55,7 +53,7 @@ lemma part_b (a b : ℤ) (hzero : a ≠ 0 ∧ b ≠ 0) (hab : a ∣ b) (hba : b 
    rw [h] at hk₂
    simp at hk₂
    assumption
-    
+
 lemma part_c (s t a b : ℤ) (h : s * a + t * b = 1) : Int.gcd a b = 1 := by 
   let M : ℤ := Int.gcd a b
   have h₁ : M ∣ a := by exact gcd_dvd_left a b
