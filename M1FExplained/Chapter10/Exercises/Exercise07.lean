32fcce8f794824ea114b4dba0e411d663d95593b
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-04 09:17:47 +0100

    wrong statment od EX07

--- M1FExplained/Chapter10/Exercises/Exercise07.lean
+++ M1FExplained/Chapter10/Exercises/Exercise07.lean
@@ -3,4 +3,18 @@ import Mathlib.Tactic
 
 namespace Chapter10.Exercise07
 
+/-
+Jim plays chess every 3 days, and his friend Marmaduke ats spaghetti every 4 days.
+One Sunday it happens that Jim plays chess and Marmaduke eats spaghetti. How long 
+will it be before this again happens on a Sunday?
+-/
+
+/-
+We can say that all three of these events happen on 'day 0'. The next day on which
+all of these three things happen simultaneously will be 'day l' where l = lcm(3, 4, 7).
+-/
+
+lemma exercise07 : {n : ℕ | 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n}.min = 84 := by 
+sorry
+
 end Chapter10.Exercise07
