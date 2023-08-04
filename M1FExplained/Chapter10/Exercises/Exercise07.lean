6d40353bdf6c8aa3e10c4b9c04f9556cc1114c91
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-04 14:04:41 +0100

    EX07 one sorry left

--- M1FExplained/Chapter10/Exercises/Exercise07.lean
+++ M1FExplained/Chapter10/Exercises/Exercise07.lean
@@ -14,7 +14,19 @@ We can say that all three of these events happen on 'day 0'. The next day on whi
 all of these three things happen simultaneously will be 'day l' where l = lcm(3, 4, 7).
 -/
 
-lemma exercise07 : {n : ℕ | 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n}.min = 84 := by 
+lemma exercise07 : IsLeast {n : ℕ | 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} 84 := by 
+  unfold IsLeast
+  constructor
+  · simp
+  · rw [mem_lowerBounds]
+    intros x hx
+    simp at hx 
+    by_contra h
+    push_neg at h
+    have h1 : x ∈ {n : ℕ | n < 84 ∧ 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} := by exact Set.mem_sep h hx 
+    have h2 : ∅ = {n : ℕ | n < 84 ∧ 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} := by sorry
+    rw [←h2] at h1 
+    contradiction
 sorry
 
 end Chapter10.Exercise07
