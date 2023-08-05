0b131baed1673737f726391220c84f57f2199856
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-05 13:53:31 +0100

    EX07 one sorry left

--- M1FExplained/Chapter10/Exercises/Exercise07.lean
+++ M1FExplained/Chapter10/Exercises/Exercise07.lean
@@ -14,19 +14,45 @@ We can say that all three of these events happen on 'day 0'. The next day on whi
 all of these three things happen simultaneously will be 'day l' where l = lcm(3, 4, 7).
 -/
 
-lemma exercise07 : IsLeast {n : ℕ | 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} 84 := by 
+lemma exercise07 : IsLeast {n : ℕ | n > 0 ∧ 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} 84 := by 
   unfold IsLeast
   constructor
   · simp
   · rw [mem_lowerBounds]
     intros x hx
-    simp at hx 
     by_contra h
     push_neg at h
-    have h1 : x ∈ {n : ℕ | n < 84 ∧ 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} := by exact Set.mem_sep h hx 
-    have h2 : ∅ = {n : ℕ | n < 84 ∧ 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} := by sorry
-    rw [←h2] at h1 
+    have h'' : x ∈ {n : ℕ | n > 0 ∧ n < 84 ∧ 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} := by sorry
+    have h' : ∅ = {n : ℕ | n > 0 ∧ n < 84 ∧ 3 ∣ n ∧ 4 ∣ n ∧ 7 ∣ n} := by 
+      rw [@Set.ext_iff]
+      intro y
+      constructor
+      · intro; exfalso; assumption
+      · intro 
+        | ⟨h0, h1, h2, h3, h4⟩ => 
+        match h2, h3, h4 with
+        | ⟨a, ha⟩, ⟨b, hb⟩, ⟨c, hc⟩ => 
+        have hb' : 3 ∣ 4 * b := by rwa [hb] at h2
+        obtain ⟨k, hk⟩ : 3 ∣ b := by 
+          · have : Prime 3 := by rw [← @Nat.prime_iff]; exact Nat.prime_three
+            apply Or.elim (this.right.right 4 b hb')
+            · intro; simp at *
+            · tauto
+        rw [hk]at hb
+        have hk' : 7 ∣ 12 * k := by rwa [hb, show (4 * (3 * k) = 12 * k) by ring] at h4
+        obtain ⟨r, hr⟩ : 7 ∣ k := by 
+          · have : Prime 7 := by rw [← @Nat.prime_iff]; exact Iff.mpr (Nat.prime_iff_card_units 7) rfl
+            apply Or.elim (this.right.right 12 k hk')
+            · intro; simp at *
+            · tauto 
+        rw [hr] at hb
+        have : y ≥ 84 := by
+          calc
+            y = 4 * (3 * (7 * r)) := by exact hb
+            _ = 84 * r := by ring
+            _ ≥ 84 * 1 := by gcongr; linarith
+        apply absurd this (show ¬y ≥ 84 by rw [@Nat.not_le]; assumption)
+    rw [←h'] at h''
     contradiction
-sorry
 
 end Chapter10.Exercise07
