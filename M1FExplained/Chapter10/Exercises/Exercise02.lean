cb9d5e8580e575d6e0bfb92d6fdc325a49f0d51c
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-07 17:18:36 +0100

    note to use EX09 in EX02

--- M1FExplained/Chapter10/Exercises/Exercise02.lean
+++ M1FExplained/Chapter10/Exercises/Exercise02.lean
@@ -15,11 +15,11 @@ lemma part_a (a b : ℤ) (ha : 0 < a) (hb : 0 < b) :
   set s' := Int.gcdA a b with rfl
   set t' := Int.gcdB a b with rfl
   set p := max ((b - s') / b) ((a + t') / a)
-  use s' + p * b, t' - p * a
+  use s' + p * b, p * a - t'
   repeat (any_goals constructor)
+  ·sorry -- use method in EX09
   ·sorry
-  ·sorry
-  ·
+  ·linear_combination this
 
 -- part b
 
