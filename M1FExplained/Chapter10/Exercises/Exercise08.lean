861301f5585bbafbcbd087ac3eae80ed144432df
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-08-08 14:20:40 +0100

    EX08 two sorries left

--- M1FExplained/Chapter10/Exercises/Exercise08.lean
+++ M1FExplained/Chapter10/Exercises/Exercise08.lean
@@ -7,13 +7,13 @@ Let n ≥ 2 be an integer. Prove that n is prime if and only if for every intege
 either gcd(a, n) = 1 or n ∣ a. 
 -/
 
-example (a n : ℤ) (hprime : Prime n) (ha :  a < n) : Int.gcd a n = 1 := by sorry
+example (m n : ℕ) (hm : 0 < m) (h : n ∣ m) : n ≤ m := by 
+  exact Nat.le_of_dvd hm h
+sorry
 
-example (a n : ℤ) (hprime : Prime n) (ha : ¬n ∣ a) : Int.gcd a n = 1 := by sorry
-#check Int.exists_prime_and_dvd
-#print Nat.Prime 
-#check Nat.prime_iff_prime_int
-#check Int.gcd_eq_natAbs
+#check Nat.prime_def_lt'
+#check Nat.prime_def_lt
+#check Nat.prime
 
 lemma exercise08' (n : ℤ) (hn : n ≥ 2) : Prime n ↔ ∀ (a : ℤ), Int.gcd a n = 1 ∨ n ∣ a := by
   rw [show n = n.natAbs by sorry] at *
@@ -22,9 +22,20 @@ lemma exercise08' (n : ℤ) (hn : n ≥ 2) : Prime n ↔ ∀ (a : ℤ), Int.gcd 
   <;> intro h 
   <;> rw [← Nat.prime_iff_prime_int] at *
   · 
-
-  -- · rw [Nat.prime_iff_prime_int] at h
-  
+    sorry
+  · rw [Nat.prime_def_lt']
+    constructor
+    · exact Iff.mp Int.ofNat_le hn
+    · rintro m hm1 hm2
+      apply Or.elim (h m)
+      <;> rintro hm3 ⟨k, hk⟩
+      · rw [hk] at hm3
+        have := Int.gcd_eq_one_of_gcd_mul_right_eq_one_left hm3
+        simp at this
+        linarith
+      · have := @Nat.le_of_dvd N m (show 0 < m by exact Nat.lt_of_succ_lt hm1) (show N ∣ m by exact Iff.mp Int.ofNat_dvd hm3)
+        linarith
+      
 
 sorry
 
